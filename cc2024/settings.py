884cbc2c7919ca4860ecdb1f2cf6732bef9a691c
mowzlisre <mowzlisre@icloud.com>
2024-11-18 12:05:32 -0500

    Postgres Creds

--- cc2024/settings.py
+++ cc2024/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'cc2024.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'labelWiseDb',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'Mowli5576',
         'HOST': 'labelwisedb.c9m2ek4uql9s.us-east-1.rds.amazonaws.com',
