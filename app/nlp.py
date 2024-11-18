5040b6d120bc8b2e25aabc87adafbc53d068187a
mowzlisre <mowzlisre@icloud.com>
2024-11-18 12:41:48 -0500

    nltk Bugs

--- app/nlp.py
+++ app/nlp.py
@@ -9,6 +9,11 @@ import os
 # Download necessary NLTK resources
 nltk.download('stopwords')
 nltk.download('punkt')
+
+nltk_data_path = "/home/ubuntu/nltk_data"
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
+
 class NLPModel:
     def __init__(self, vectorizer_path, model_path):  # Corrected from _init_ to __init__
         # Load the vectorizer and model
